81f759518df7c9fdf8d34b3b5f42a76bcd668be5
Som Mood <sommood55@gmail.com>
2011-09-02 00:02:13 +0700

    Initial Commit

---
+++ myOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myOS
+//
+//  Created by Tix Tox on 9/2/54 BE.
+//  Copyright 2554 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Tix Tox on 9/2/54 BE.
+//  Copyright 2554 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
